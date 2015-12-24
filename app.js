9af161f5b6e56c2430a7810a82d776a13a631ed4
unknown <v-mare@MININT-6T8FCKE.redmond.corp.microsoft.com>
2015-12-23 16:31:17 -0800

    switching to front end

--- app.js
+++ app.js
@@ -1,27 +1,46 @@
-﻿var request = require("request");
+﻿var pg = require('pg');
+var request = require("request");
 var cheerio = require("cheerio");
 const dayone = new Date(2015, 9, 27);
 const today = new Date();
 var total = Date.parse(today) - Date.parse(dayone);
 var numday = Math.ceil((total)  /  (1000  *  3600  *  24));
 
-var promises = [];
+job(1, 12);
 
-for (var i = 1; i <= 12; i++) {
-    promises.push(new Promise(function (resolve, reject) {
-        var index = i;
-        request.get('http://games.espn.go.com/fba/boxscorequick?leagueId=273247&teamId=' + index + '&scoringPeriodId=' + numday + '&seasonId=2016&view=matchup', function (err, res) {
-            if (err) reject(false);            
-            var html = res.body;
-            var $ = cheerio.load(html);
-            var score = $("#tmTotalPtsRaw_" + index)[0].children[0].data;
-            var games = $("#weekpace_" + index + "_0")[0].children[0].data;
-            console.log("teamid: "+index+" "+(score / games).toPrecision(4));
-            resolve(true);
+function job(start, end) {
+    var promises = [];
+    for (var i = start; i <= end; i++) {
+        promises.push(new Promise(function (resolve, reject) {
+            var index = i;
+            request.get('http://games.espn.go.com/fba/boxscorequick?leagueId=273247&teamId=' + index + '&scoringPeriodId=' + numday + '&seasonId=2016&view=matchup', function (err, res) {
+                if (err) reject(false);
+                var html = res.body;
+                var $ = cheerio.load(html);
+                var team = {};
+                team.score = $("#tmTotalPtsRaw_" + index)[0].children[0].data;
+                team.games = $("#weekpace_" + index + "_0")[0].children[0].data;
+                team.avgscore = (team.score / team.games).toPrecision(4);
+                team.id = index;
+                resolve(team)
+            });
+        }));
+    }
+    Promise.all(promises).then(function (values) {
+        values.forEach(function (v, i) {
+            pg.connect(process.env.DATABASE_URL, function (err, client, done) {
+                if (err) console.log(err);
+                console.log('Connected to postgres! Getting schemas...');
+                console.log('Attempting to update id: ' + v.id);
+                client
+                    .query("update fantasybasketball.stats set averageScore = '" + v.avgscore + "', totalscore= '" + v.score + "', gamesplayed = '" + v.games + "' where id ='" + v.id + "';")
+                    .on('row', function (row) {
+                    done();
+                    console.log('Successfully updated team with ID: ' + v.id);
+                })
+                    .on('error', function (e) { console.log(e) })
+                    .on('end', function (d) { console.log(d) });
+            });
         });
-    }));
+    });
 }
-
-Promise.all(promises).then(function (values) { 
-    console.log();
-});
---
+++ data.js
@@ -0,0 +1,19 @@
+﻿var pg = require('pg');
+
+function data() { }
+
+data.prototype.update = function update(team) {
+    pg.connect(process.env.DATABASE_URL, function (err, client, done) {
+        if (err) console.log(err);
+        console.log('Connected to postgres! Getting schemas...');
+        console.log('Attempting to update id: ' + team.id);
+        client
+        .query("update fantasybasketball.stats set averageScore = '" + team.avgscore + "', totalscore= '" + team.score + "', gamesplayed = '" + team.games + "' where id ='" + team.id + "';")
+        .on('row', function (row) {
+            done();
+            console.log('Successfully updated team with ID: ' + team.id);
+        }).on('error', function (e) { console.log(e) });
+    });
+}
+
+module.exports = data;
